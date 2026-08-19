832d27c0fce294880afb918f686119bac179660c
yohanpcscommunaute-code <yohanpcscommunaute@gmail.com>
2026-08-19 01:56:36 +0000

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ if not BOT_TOKEN:
         "TELEGRAM_TOKEN n'est pas configuré."
     )
 
+
+# MEGAPARI
 MEGAPARI_REGISTER_URL = os.getenv(
     "https://3773080.megapari-104631.in/",
     ""
@@ -22,4 +24,30 @@ MEGAPARI_MIN_DEPOSIT = float(
         "MEGAPARI_MIN_DEPOSIT",
         "25"
     )
+)
+
+
+# PREDICTIONS
+TIME_MINUTES_MIN = int(
+    os.getenv("TIME_MINUTES_MIN", "1")
+)
+
+TIME_MINUTES_MAX = int(
+    os.getenv("TIME_MINUTES_MAX", "5")
+)
+
+ODDS_MIN = float(
+    os.getenv("ODDS_MIN", "1.50")
+)
+
+ODDS_MAX = float(
+    os.getenv("ODDS_MAX", "3.00")
+)
+
+SAFE_MIN = int(
+    os.getenv("SAFE_MIN", "70")
+)
+
+SAFE_MAX = int(
+    os.getenv("SAFE_MAX", "95")
 )
